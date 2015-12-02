a0bdef88b72049559dd4b3d6cb006a6f281ee2e3
Josef Bacik <jbacik@fb.com>
2015-12-02 10:43:58 -0500

    trace-tools: add license crap
    Needed to add this to the rest of the files.
    
    Signed-off-by: Josef Bacik <jbacik@fb.com>

--- trace-event-sorter.h
+++ trace-event-sorter.h
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Facebook, Josef Bacik <jbacik@fb.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License (not later!)
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not,  see <http://www.gnu.org/licenses>
+ */
 #ifndef __TRACE_EVENT_SORTER_H__
 #define __TRACE_EVENT_SORTER_H__
 
--- trace-syscalls.c
+++ trace-syscalls.c
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Facebook, Josef Bacik <jbacik@fb.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License (not later!)
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not,  see <http://www.gnu.org/licenses>
+ */
 #include <sys/types.h>
 #include <stdlib.h>
 #include <signal.h>
